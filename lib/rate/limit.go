6d534eae26a4bfb29c34eb76379103f278964c29
wike <>
2024-04-26 14:46:44 +0800

    添加熔断和限流

--- lib/rate/limit.go
+++ lib/rate/limit.go
@@ -3,9 +3,14 @@ package rate
 import (
 	"github.com/spf13/viper"
 	"github.com/wike2019/wike_go/lib/rateLimiter"
+	"golang.org/x/time/rate"
 )
 
 func Limit(cfg *viper.Viper) *rateLimiter.RateLimiterCache {
 	limit := cfg.GetInt("LRULimit")
 	return rateLimiter.NewRateLimiterCache(limit)
+}
+
+func CreateLimiter(r rate.Limit, b int) *rate.Limiter {
+	return rate.NewLimiter(r, b)
 }
